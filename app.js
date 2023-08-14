420f8d5360e51d7fa474063f9ee9cbc35a4d997d
RaghavenderMaram <124790249+RaghavenderMaram@users.noreply.github.com>
2023-08-14 12:12:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+var hr=document.getElementById('hour');
+var min=document.getElementById('minutes');
+var sec=document.getElementById('seconds');
+var ampm1=document.getElementById('ampm');
+function updateclock()
+{
+    var date=new Date();
+    h=date.getHours();
+    m=date.getMinutes();
+    s=date.getSeconds();
+    ampm2="AM";
+    if(h>12){
+    h=h-12;
+    ampm2="PM";
+}
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+
+    hr.innerText=h;
+    min.innerText=m;
+    sec.innerText=s;
+    ampm1.innerText=ampm2;
+    setTimeout(updateclock,1000);
+}
+updateclock();
+
+
